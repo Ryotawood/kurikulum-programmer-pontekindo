5eb6e483ae259899434eb556d14600f1ff495802
Ryotawood <storagefaqs@gmail.com>
2023-07-13 08:09:32 +0700

    lagi

--- sprint1/tugas/tugas1.php
+++ sprint1/tugas/tugas1.php
@@ -16,3 +16,29 @@ $keliling = 2 * $panjang + 2*$lebar;
 echo "Luas persegi panjang". $luas . "\n";
 echo "Keliling persegi panjang: " . $keliling . "\n";
 
+echo "\n===== SEGITIGA =====\n";
+echo "Masukkan alas :";
+$alas = trim(fgets(STDIN));
+echo "Masukkan tinggi :";
+$tinggi = trim(fgets(STDIN));
+echo "Masukkan sisi 1 :";
+$sisi1 = trim(fgets(STDIN));
+echo "Masukkan sisi 2 :";
+$sisi2 = trim(fgets(STDIN));
+echo "Masukkan sisi 3 :";
+$sisi3 = trim(fgets(STDIN));
+    $luas = 0.5 * $alas * $tinggi;
+    $keliling = $sisi1 + $sisi2 + $sisi3;
+
+    echo "Luas segitiga: " . $luas . "\n";
+    echo "Keliling segitiga: " . $keliling . "\n";
+    
+    echo "\n===== LINGKARAN =====\n";
+    echo "Masukkan jari-jari: ";
+    $jariJari = trim(fgets(STDIN));
+
+    $luas = 3.14 * $jariJari * $jariJari;
+    $keliling = 2 * 3.14 * $jariJari;
+
+    echo "Luas lingkaran: " . $luas . "\n";
+    echo "Keliling lingkaran: " . $keliling . "\n";
